9c9549b0d6a217bc2e80e75936f26a5c7b4f93b8
Tanner Bernth <tannerbernth@dhcp-10-142-183-161.uawifi.arizona.edu>
2016-11-15 12:39:39 -0700

    Added basic world/generation and corresponding test case

---
+++ src/world/TestWorld.java
@@ -0,0 +1,17 @@
+package world;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestWorld {
+	
+	@Test
+	public void testWorldGeneration() {
+		World world = World.getWorld();
+		int zerozero = world.getTile(0, 0);
+		world.setTile(0, 0, 2);
+		assertFalse(world.getTile(0, 0) == zerozero);
+		world = world.getWorld();
+		assertFalse(world.getTile(0, 0) == zerozero);
+	}
+}
