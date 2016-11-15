d88039156bb9feef3ab2db5a339d9e465e29f737
robax <robertwalters@email.arizona.edu>
2016-11-15 13:14:11 -0700

    tileset is a singleton

--- src/entities/RenderComponent.java
+++ src/entities/RenderComponent.java
@@ -6,11 +6,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-// TODO: ensure tileset is a singleton
-
 public class RenderComponent extends Component {
-
-	private static final String TILESET_PATH = "gfx/tiles.png";
 
 	public RenderComponent(BufferedImage texture, int width, int height) {
 		super(RENDER);
@@ -32,26 +28,20 @@ public class RenderComponent extends Component {
 
 	public RenderComponent(String pic){
 		super(RENDER);
-		// TODO: parse the JSON file and get the picture coordinates
-		// instead of hard-coding it
-		try{
-			BufferedImage tileset = ImageIO.read(new File(TILESET_PATH));
-			switch(pic){
-			case "alien":
-				this.texture = tileset.getSubimage(7*32, 15*32, 32, 32);
-				break;
-			case "ammo":
-				this.texture = tileset.getSubimage(32, 32, 32, 32);
-				break;
-			case "godzilla":
-				this.texture = tileset.getSubimage(4*32, 19*32, 32, 32);
-				break;
-			case "bandage":
-				this.texture = tileset.getSubimage(0, 13*32, 32, 32);
-				break;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		BufferedImage tileset = Tileset.instance().tileset();
+		switch(pic){
+		case "alien":
+			this.texture = tileset.getSubimage(7*32, 15*32, 32, 32);
+			break;
+		case "ammo":
+			this.texture = tileset.getSubimage(32, 32, 32, 32);
+			break;
+		case "godzilla":
+			this.texture = tileset.getSubimage(4*32, 19*32, 32, 32);
+			break;
+		case "bandage":
+			this.texture = tileset.getSubimage(0, 13*32, 32, 32);
+			break;
 		}
 		this.width = 32;
 		this.height = 32;
