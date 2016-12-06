297e1ef50f9c09c2a19645fba8ec90bc3410b97f
Caleb Short <Caleb@dhcp-10-134-246-31.uawifi.arizona.edu>
2016-12-06 15:35:11 -0700

    Working on fatigue

--- src/entities/LivingSystem.java
+++ src/entities/LivingSystem.java
@@ -42,12 +42,7 @@ public class LivingSystem extends System {
       lc.HP -= 15.0f / TICKS_PER_SECOND;
     else if(lc.hydration < 10.0f)
       lc.HP -= 30.0f / TICKS_PER_SECOND;
-    lc.hydration -= 1.0f / TICKS_PER_SECOND;
-    
-    if(lc.hydration >= 80.0f)
-      lc.HP += 10.0f / TICKS_PER_SECOND;
-    if(lc.HP > lc.maxHP)
-      lc.HP = lc.maxHP;
+    lc.hydration -= 0.5f / TICKS_PER_SECOND;
     
     World w = World.getWorld();
     int y = Math.round(pc.pos.y);
@@ -71,23 +66,25 @@ public class LivingSystem extends System {
 	      lc.HP -= 30.0f / TICKS_PER_SECOND;
 	    lc.restVal -= 0.1f / TICKS_PER_SECOND;
 	    
-	    if(lc.hydration >= 80.0f)
-	      lc.HP += 10.0f / TICKS_PER_SECOND;
-	    if(lc.HP > lc.maxHP)
-	      lc.HP = lc.maxHP;
-	    
 	    World w = World.getWorld();
 	    int y = Math.round(pc.pos.y);
 	    int x = Math.round(pc.pos.x);
 	    y = y < 0 ? 0 : y > w.getSize() ? w.getSize() : y;
 	    x = x < 0 ? 0 : x > w.getSize() ? w.getSize() : x;
-	    if(w.getTile(y, x).getType() == Sprite.LAKE)
-	      lc.hydration += 30.0f / TICKS_PER_SECOND;
+	    if(w.getTile(y, x).getType() == Sprite.HOUSE || w.getTile(y, x).getType() == Sprite.SHIP)
+	      lc.restVal += 6.0f / TICKS_PER_SECOND;
 
-	    if(lc.hydration > lc.maxHydration)
-	      lc.hydration = lc.maxHydration;
-	    //java.lang.System.out.println(lc.hydration);
-	  }
+	    if(lc.restVal > lc.restVal)
+	      lc.restVal = lc.restVal;
+	    //java.lang.System.out.println(lc.restVal);
+  }
+  
+  private void regainHealth(LivingComponent lc, PositionComponent pc){
+	  if(lc.hydration >= 80.0f && lc.restVal >= 80.0f)
+	    lc.HP += 5.0f / TICKS_PER_SECOND;
+	  if(lc.HP > lc.maxHP)
+	    lc.HP = lc.maxHP;
+  }
 
   private void purgeTheDead(Entity e){
     PositionComponent ps =
